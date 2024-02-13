cbc17be60bdf8e3cd2bf1a7140eeab753259b25a
jaewoneee <jaewon810@naver.com>
2024-02-13 17:01:10 +0900

    build: eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'universe/native',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+};
