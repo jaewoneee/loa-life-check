8d4ee76e1136504b1a6d1dba5a8b764ff4ecedaf
jaewoneee <jaewon810@naver.com>
2024-02-29 10:51:33 +0900

    feat: new types for custom theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,27 +1,29 @@
-import { Theme } from '@react-navigation/native';
-
-const DefaultTheme: Theme = {
+const DefaultTheme = {
   dark: false,
   colors: {
     primary: 'rgb(20, 20, 20)',
     background: 'rgb(255, 255, 255)',
     text: 'rgb(31, 31, 31)',
-    card: 'rgb(18, 18, 18)',
-    border: 'rgb(25,25,25)',
+    card: 'rgb(1, 42, 65)',
+    border: '',
     notification: 'rgb(255, 69, 58)',
+    point: 'rgb(1, 76, 117)',
   },
 };
 
-const DarkTheme: Theme = {
+const DarkTheme = {
   dark: true,
   colors: {
     primary: 'rgb(255, 255, 255)',
-    background: 'rgb(8, 8, 8)',
+    background: 'rgb(0, 17, 26)',
     text: 'rgb(200, 200, 200)',
-    card: 'rgb(18, 18, 18)',
-    border: 'rgb(50, 50, 50)',
+    card: 'rgb(1, 42, 65)',
+    border: 'rgb(0, 34, 52)',
     notification: 'rgb(255, 69, 58)',
+    point: 'rgb(1, 76, 117)',
   },
 };
+
+export type Theme = typeof DefaultTheme;
 
 export { DefaultTheme, DarkTheme };
